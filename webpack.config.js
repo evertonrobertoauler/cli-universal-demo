827f8f74cf33bde34b59a733bfc5caac60e95a66
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-04-07 11:49:01 -0300

    Replace ts-node with webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    server: './src/server.ts'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  target: 'node',
+  node: {
+    __dirname: true
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+}
