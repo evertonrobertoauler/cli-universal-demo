61177ff88916e3824c1c01bf276830b97a9a8ddf
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-08-16 21:32:05 -0300

    Adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'about', component: AboutComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: '' }
+    ])
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/sw-precache-config.js
+++ src/sw-precache-config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   stripPrefix: 'dist/browser',
   runtimeCaching: [{
-    urlPattern: '/',
+    urlPattern: /(\/|\/about)$/,
     handler: 'networkFirst'
   }]
 };
