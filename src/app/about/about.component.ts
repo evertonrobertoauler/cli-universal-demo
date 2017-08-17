29375f1412933a9261fcce124d997f3577fb4bb1
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-08-16 21:43:03 -0300

    Adding SEO tags

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _title: Title, private _meta: Meta) { }
 
   ngOnInit() {
+    this._title.setTitle('About Page');
+    this._meta.updateTag({ name: 'description', content: 'About Page Description' });
   }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _title: Title, private _meta: Meta) { }
 
   ngOnInit() {
+    this._title.setTitle('Home Page');
+    this._meta.updateTag({ name: 'description', content: 'Home Page Description' });
   }
 
 }
